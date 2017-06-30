7b05e9d7f0c1141babf2d303e022cab22bde8099
Biguá <bigua.kun@gmail.com>
2017-06-30 01:03:55 -0300

    editando o readme

--- eva01/bootstrap.sh
+++ eva01/bootstrap.sh
@@ -6,7 +6,7 @@ tar -xvzf multichain-1.0-beta-2.tar.gz
 cd multichain-1.0-beta-2
 sudo mv multichaind multichain-cli multichain-util /usr/local/bin
 
-multichain-util create colecao  -'anyone-can-connect'=true -'anyone-can-send'=true -'anyone-can-receive'=true -'anyone-can-create'=true -'default-rpc-port'=6666
+multichain-util create colecao  -'anyone-can-connect'=true -'anyone-can-send'=true -'anyone-can-receive'=true -'anyone-can-create'=true  -'default-rpc-port'=6666 -'default-network-port'=6665
 cd /home/vagrant/.multichain/colecao/
 sed -i 's/MultiChain\ colecao/Minha\ colecao\ de\ gibi/' params.dat
 multichaind colecao -daemon
